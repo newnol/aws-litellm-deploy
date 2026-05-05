693bd5212c3d8b2a537e45353d2440cb9ef85809
Ngô Tấn Tài <tantai13102005@gmail.com>
2026-05-05 07:25:51 +0700

    refactor: simplify modules/security-groups/outputs.tf for EC2 only

--- modules/security-groups/outputs.tf
+++ modules/security-groups/outputs.tf
@@ -1,14 +1,4 @@
-output "alb_sg_id" {
-  description = "ALB security group ID"
-  value       = aws_security_group.alb.id
-}
-
-output "ecs_sg_id" {
-  description = "ECS security group ID"
-  value       = aws_security_group.ecs.id
-}
-
-output "rds_sg_id" {
-  description = "RDS security group ID"
-  value       = aws_security_group.rds.id
+output "ec2_sg_id" {
+  description = "EC2 security group ID"
+  value       = aws_security_group.ec2.id
 }
